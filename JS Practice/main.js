d9c9342e76cec85ef752fae06281425155afd77b
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-18 18:05:26 +0600

    CALL METHOD

--- JS Practice/main.js
+++ JS Practice/main.js
@@ -1,21 +1,25 @@
-const button = document.querySelector('button'),
-      result = document.getElementById('result');
-
-      button.addEventListener('click',getResult);
 
 
-function getResult(){
-  let request = new XMLHttpRequest();
-request.open('GET','http://jsonplaceholder.typicode.com/posts',true);
-request.send();
-request.onreadystatechange = function handleRequest(){
-  if(request.readyState === 4 && request.status === 200){
-    let info = JSON.parse(request.responseText).map(
-      s=>s.title
-    );
-    console.log(info)
-    result.innerHTML = info;
+// Genuine Object
+
+let mainObj = {
+  name : 'Main Object',
+  importance: 1,
+  childObj:{
+    name :'Child Object',
+    importance :2,
+    output(){
+      console.log('I\'m ' + this.name)
+    }
   }
 }
-  
-}
+// when we call the output method of the childObj 'this' returning its nearest object on parent named childObj
+console.log(mainObj.childObj.output())  // I'm child Object
+
+// What if we had to call the mainObj ?
+// On this circumstance js built in method call() comes handy ..
+
+// Call()
+
+
+console.log(mainObj.childObj.output.call(mainObj)) // I'm Main Object
