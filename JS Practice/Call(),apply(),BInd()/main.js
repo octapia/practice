b2f70201a20e6d44ecfd5224199c816c8136e1bb
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-19 13:46:38 +0600

    call() ,apply() and bind() method

--- JS Practice/Call(),apply(),BInd()/main.js
+++ JS Practice/Call(),apply(),BInd()/main.js
@@ -1,18 +1,27 @@
-let user1 = {
-  name : 'MR X ',
-  expertise : 'html,css,js',
-  isExpertAt(){
-    console.log('I am expert at : ' ,this.expertise)
-  }
+//call(), bind() and apply() method 
+var user = {
+  name : 'Windows 10',
+  id : '10'
 }
 
-user1.isExpertAt();
-// using call() method we can apply a function for other object 
-// below , we arr calling  user2 using method from user1
+let self = {
+  name : 'Tahidur Rahman',
+  id   : 'none'
+}
+let self2 = {
+  name : 'Sahidur Rahman',
+  id   : 1
+}
 
-let user2 = {
-  name : 'MR X ',
-  expertise : 'html,css,js,react'
-  }
+let func = function (para){
+  let me = `I am ${this.name} with an id of ${this.id}.`
+  console.log(me,para);
+}
+// Call() method 
+func.call(self2);
+// bind() method
+func.bind(self)();
+// bind() method doesn't take additional parameter
 
-  user1.isExpertAt.call(user2)
+// apply method
+func.apply(self2);
