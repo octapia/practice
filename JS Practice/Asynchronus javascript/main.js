0e8e253def7f1ecd5dbf39c325f99ff9fd7b05f5
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-20 19:30:21 +0600

    Promise

--- JS Practice/Asynchronus javascript/main.js
+++ JS Practice/Asynchronus javascript/main.js
@@ -1,46 +1,23 @@
+// Promise
 
-// MAKING of a web API using callback in AJAX
-
-function getResponse(url,callback){
-    let xhr = new XMLHttpRequest();
-    xhr.open('get',url,true);
-    xhr.onreadystatechange = function(e){
-        if(xhr.status === 200 && xhr.readyState === 4){
-            let response = JSON.parse(xhr.response);
-            callback(null,response);
-        }else{
-            callback(xhr.status,null)
-        }
-    }
-    xhr.send();
+function getIphone(isPassed){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            
+            if(isPassed){
+                resolve('Le beta... Iphone le...');
+            }else{
+                reject('Aah.. beta Pitai.. Kha ja..')
+            }
+        },2000)
+    })
 }
 
-// callback is not always wise for async js 
-// let's realise why ?
+getIphone(true)
+.then(res => console.log(res))
+.catch(e=>console.log(e));
 
-// we below we will call some different data from server
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+getIphone(false)
+.then(res => console.log(res))
+.catch(e=>console.log(e));
 
-getResponse(`${BASE_URL}/posts/1`,(err,res)=>{
-    if(err){
-        throw new Error('Something is wrong')
-    }else{
-        console.log(res.title)
-    }
-})
-getResponse(`${BASE_URL}/users/1`,(err,res)=>{
-    if(err){
-        throw new Error('Something is wrong')
-    }else{
-        console.log(res.name)
-    }
-})
-getResponse(`${BASE_URL}/posts/1`,(err,res)=>{
-    if(err){
-        throw new Error('Something is wrong')
-    }else{
-        console.log(res.title)
-    }
-})
-
-//  it could be too tough to maintain a lots of callback for asynch js
