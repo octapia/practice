624d57031db9f92629ad766866edbe9f700077b6
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-20 12:58:22 +0600

    AJAX Request and Callback for async JS

--- JS Practice/Asynchronus javascript/main.js
+++ JS Practice/Asynchronus javascript/main.js
@@ -1,31 +1,42 @@
 
-// CallBack JS
+// //Simple AJAX request 
 
-let users = ["Tahid",'Shohid'];
+// let xhr = new XMLHttpRequest();
 
+// xhr.open('get','https://jsonplaceholder.typicode.com/posts');
 
-function firstUser (){
-    setTimeout(function(){
-        document.body.innerHTML = `First user ${users[0]}`
-    },1000)
+// xhr.onreadystatechange = function(e){
+//     if(xhr.readyState === 4 && xhr.status === 200){
+//         let users = JSON.parse(xhr.response);
+//         console.log(users)
+//     }else{
+//         console.log(xhr.readyState ,xhr.status)
+//     }
+// }
+
+// xhr.send()
+
+// MAKING of a web API using callback in AJAX
+
+function getResponse(url,callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open('get',url,true);
+    xhr.onreadystatechange = function(e){
+        if(xhr.status === 200 && xhr.readyState === 4){
+            let response = JSON.parse(xhr.response);
+            callback(null,response);
+        }else{
+            callback(xhr.status,null)
+        }
+    }
+    xhr.send();
 }
 
-
-function secondUser(){
-    setTimeout(function(){
-       document.body.innerHTML += `<br>Second user ${users[1]}`
-    },2000)
-}
-
-
-
-function thirdUser(newUser,callBack1,callBack2){
-    setTimeout(function(){
-        users.push(newUser);
-        callBack1();
-        callBack2();
-       document.body.innerHTML += `<br>Third user ${users[2]}`
-    },3000)
-}
-
-thirdUser('Aziz',firstUser,secondUser;)
+// Now we can call for the response for any url ...
+getResponse('https://jsonplaceholder.typicode.com/users',(err,res)=>{
+    if(err){
+        console.log(err)
+    }else{
+        res.forEach(r=>console.log(r.name));
+    }
+})
