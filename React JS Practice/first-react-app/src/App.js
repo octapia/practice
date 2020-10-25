b1df365ad11a665d94dc4c9d7b92c0051a8b8a7c
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-25 22:54:37 +0600

    useEffect()

--- React JS Practice/first-react-app/src/App.js
+++ React JS Practice/first-react-app/src/App.js
@@ -1,22 +1,23 @@
-import React , { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
-function App (){
-    const [name,setName] = useState("Tahid");
-    const [roll, setroll] = useState(11)
-
-    const clickHandle = ()=>{
-        setName('rahul');
-        setroll(22)
-    }
+function App(){
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(20);
+    
+    const changeCount = () => setCount(count + 1);
+    const changeCount2 = () => setCount2(count2 - 1);
+    useEffect( ()=>{
+        console.log('Effect Done !')
+    },[count2,count])
 
     return (
         <>
-        <h1>Name : {name}</h1>
-        <h1>roll : {roll}</h1>
-
-        <button onClick={clickHandle}>Change</button>
+        <h1>Count : {count}</h1>
+        <button onClick={changeCount}>ChangeCount</button>
+        <h1>Count2 : {count2}</h1>
+        <button onClick={changeCount2}>ChangeCount2</button>
         </>
     )
-}
 
+}
 export default App;
