f257b16a91271da26e37ce0269f31aac5a1b5540
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-10-27 00:26:48 +0600

    rest api usign node,express,mongoose few changes made

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -3,8 +3,11 @@ const Post = require(`../models/PostModel`);
 /* get all */
 router.get(`/`, async (req, res) => {
     try {
-        const list = await Post.find();
-        res.json(list);
+        const getAll = await Post.find();
+        getAll
+            ?
+            res.json({ message: `Data list`, success: true, status: 200, data: getAll }) :
+            res.json({ message: `Not found`, success: false, status: 404 })
     } catch (error) {
         res.json({ message: error.message, success: false, status: 404 })
     }
@@ -15,7 +18,10 @@ router.get(`/`, async (req, res) => {
 router.get(`/:id`, async (req, res) => {
     try {
         const getSingle = await Post.findById(req.params.id);
-        res.json(getSingle);
+        getSingle
+            ?
+            res.json({ message: `Single data`, success: true, status: 200, data: getSingle }) :
+            res.json({ message: `Not found`, success: false, status: 404 })
     } catch (error) {
         res.json({ message: error.message, success: false, status: 404 })
     }
@@ -32,7 +38,7 @@ router.post(`/`, async (req, res) => {
     });
     try {
         const createdNew = await post.save();
-        res.json(createdNew);
+        res.json({ message: `Created successfully`, success: true, status: 200, data: createdNew });
     } catch (error) {
         res.json({ message: error.message, success: false, status: 500 })
     }
@@ -46,12 +52,14 @@ router.put(`/:id`, async (req, res) => {
     let { title, body } = req.body;
     try {
         const getSingle = await Post.findById(id);
-
         title = title || getSingle.title;
         body = body || getSingle.body;
 
-        const updateSingle = await Post.findOneAndUpdate({ _id: id }, { $set: { title, body } }, { new: true });
-        res.json(updateSingle);
+        const updateSingle = await Post.findByIdAndUpdate(id, { $set: { title, body } }, { new: true });
+        updateSingle
+            ?
+            res.json({ message: `Updated successfully`, success: true, status: 200, data: updateSingle }) :
+            res.json({ message: `Not found`, success: false, status: 404 })
     } catch (error) {
         res.json({ message: error.message, success: false, status: 500 })
     }
@@ -63,8 +71,10 @@ router.put(`/:id`, async (req, res) => {
 router.delete(`/:id`, async (req, res) => {
     const { id } = req.params;
     try {
-        const deleteSingle = await Post.findOneAndDelete(id);
-        res.json({ message: `Deleted successfully`, success: true, status: 200, data: deleteSingle });
+        const deleteSingle = await Post.findByIdAndDelete(id);
+        deleteSingle ?
+            res.json({ message: `Deleted successfully`, success: true, status: 200, data: deleteSingle }) :
+            res.json({ message: `Not found`, success: false, status: 404 })
 
     } catch (error) {
         res.json({ message: error.message, success: false, status: 500 })
