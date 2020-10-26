4da779ae3496faaac3af89e7562948ba8a9702a1
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-26 20:59:21 +0600

    controlled components in form

--- React JS Practice/first-react-app/src/App.js
+++ React JS Practice/first-react-app/src/App.js
@@ -3,11 +3,34 @@ import React, { Component } from 'react';
 
 
 export default class App extends Component {
+    state = {
+        name : 'TAHID',
+        password :'pass'
+    }
+    clickHandle = (e)=> {
+        this.setState({[e.target.name] : e.target.value.toUpperCase().substring(0,10)})
+    }
     render() {
         return (
-            <div>
-                <button className="btn btn-primary">Button</button>
-            </div>
+            <form className='form p-5 '>
+                <label>Name :
+                    <input 
+                    type="text" 
+                    name="name"
+                    value={this.state.name}
+                    className='m-2 p2 o-none'
+                    onChange={this.clickHandle}/>
+                </label>
+                <label>Password :
+                    <input 
+                    type="password" 
+                    name="password"
+                    value={this.state.password}
+                    className='m-2 p2'
+                    onChange={this.clickHandle}/>
+                </label>
+                <button>Submit</button>
+            </form>
         )
     }
 }
