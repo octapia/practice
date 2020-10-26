5dde2db8a0d47fd3e4638454514488e9725cd5b0
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-26 19:11:16 +0600

    assets connection in react

--- React JS Practice/first-react-app/src/App.js
+++ React JS Practice/first-react-app/src/App.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react'
+
+import React, { Component } from 'react';
+
 
 export default class App extends Component {
-    state = {
-        change :false
-    }
-    btnChange = () => this.setState({change:true})
-
     render() {
-        const btn = {
-            color: 'white',
-            backgroundColor: 'green',
-            fontSize:'42px'
-        }
-        if(this.state.change) btn.backgroundColor = 'red';
-        
         return (
-            <button onClick={this.btnChange} style={btn}>
-                Button
-            </button>
+            <div>
+                <img src={process.env.PUBLIC_URL + '/demo.png'}
+                width="400px" />
+            </div>
         )
     }
 }
