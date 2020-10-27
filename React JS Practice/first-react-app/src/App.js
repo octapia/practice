3a85af1a313a7bf226e8d13d7ce874d1fe038b1f
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-10-27 14:48:17 +0600

    Uncontrolled component ref

--- React JS Practice/first-react-app/src/App.js
+++ React JS Practice/first-react-app/src/App.js
@@ -1,36 +1,33 @@
-
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
 
 export default class App extends Component {
-    state = {
-        name : 'TAHID',
-        password :'pass'
+    constructor(props){
+        super(props);
+        this.state = {
+            value : ''
+        };
+        this.textInput = React.createRef();
     }
-    clickHandle = (e)=> {
-        this.setState({[e.target.name] : e.target.value.toUpperCase().substring(0,10)})
+    // componentDidMount = () => {
+    //    this.textInput.current.focus()
+    // }
+    
+    getInput = e =>{
+        e.preventDefault();
+
+        this.setState({value:this.textInput.current.value})
     }
+
     render() {
         return (
-            <form className='form p-5 '>
-                <label>Name :
-                    <input 
-                    type="text" 
-                    name="name"
-                    value={this.state.name}
-                    className='m-2 p2 o-none'
-                    onChange={this.clickHandle}/>
-                </label>
-                <label>Password :
-                    <input 
-                    type="password" 
-                    name="password"
-                    value={this.state.password}
-                    className='m-2 p2'
-                    onChange={this.clickHandle}/>
-                </label>
-                <button>Submit</button>
-            </form>
+            <div>
+                <form className="p-2" onSubmit={this.getInput}>
+              
+                Input : <input type="text" ref={this.textInput} /><br/> <br/>
+                <input type="submit" value="Submit" />
+                </form>
+                  You Typed : {this.state.value} <br/>
+                </div>
         )
     }
 }
