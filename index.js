57bf33252ba90e791d605b758098ac83359715a4
MDIftakherHossain <67021268+MDIftakherHossain@users.noreply.github.com>
2020-10-31 06:25:53 +0600

    sort, find, create, drop collection

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 
 
 const MongoClient = require('mongodb').MongoClient
-const url ='mongodb://localhost/alienDBex'
-const config = {useUnifideTopology:true}
+const url ='mongodb://127.0.0.1:27017/'
+const config = {useUnifiedTopology:true}
 
 MongoClient.connect(url , config,(error,MyMongoClient) =>{
 if(error){
-  console.log('connected fail')
+  console.log('connection fail')
 }else{
-console.log(`connected`);
-InsertData(MyMongoClient);
-DeleteOneItem(MyMongoClient)
+console.log(`connected with MongoDB`);
+// InsertData(MyMongoClient);
+// DeleteOneItem(MyMongoClient)
+// DeleteAllItem(MyMongoClient)
+// FindOneItem(MyMongoClient)
+// FindAllItem(MyMongoClient)
+// FindByProjection(MyMongoClient)
+// FindAllByQuery(MyMongoClient)
+// FindDataByLimit(MyMongoClient)
+// FindDataBySort(MyMongoClient)
+// UpdateOneData(MyMongoClient)
+// CreateCollection(MyMongoClient)
+DropCollection(MyMongoClient)
+
 }
 })
 
 //  InsertData
-
 function InsertData(MyMongoClient){
   var MyDB = MyMongoClient.db('school')
   var MyCollection = MyDB.collection('students')
   var MyData = {
-    name:"Iftakher",
-    Roll:"01",
-    Class:"One"
+    name:"Jasim",
+    Roll:"02",
+    Class:"Two"
   };
   MyCollection.insertOne(MyData,(error)=>{
   if(error){
@@ -34,17 +44,142 @@ function InsertData(MyMongoClient){
 }
 
 // DeleteOneItem
-
 function DeleteOneItem(MyMongoClient){
   var MyDB = MyMongoClient.db('school')
   var MyCollection = MyDB.collection('students')
-  var DeleteItem = { Roll:"01" }
+  var DeleteItem = { Roll:"03" }
   
   MyCollection.deleteOne(DeleteItem,(error)=>{
   if(error){
   console.log('Data delete fail')
   }else{
-  console.log(`Data deletde`);
+  console.log(`Data deleted`);
   }
 })
 }
+
+// delete all Items
+
+function DeleteAllItem(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  var MyCollection = MyDB.collection('teachers')
+  
+  MyCollection.deleteMany((error,ResultObj)=>{
+  if(error){
+  console.log('Delete fail')
+  }else{
+  console.log(ResultObj.result.n + "Items deleted");
+  }
+})
+}
+
+// find one item
+
+function FindOneItem(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  var MyCollection = MyDB.collection('students')
+  const FindObj = {"Roll":"02"}
+  MyCollection.findOne(FindObj,(error,result)=>{
+  if(error){
+  console.log('Error')
+  }else{
+  console.log(result);
+  }
+})
+}
+
+// find All item
+
+function FindAllItem(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  var MyCollection = MyDB.collection('students')
+
+  MyCollection.find().toArray((error,result)=>{
+   console.log(result)
+})
+}
+
+// find By projection
+
+function FindByProjection(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  var MyCollection = MyDB.collection('students')
+  const ItemObj ={}
+  const ItemProjection = {projection:{"_id":0,Class:1, Roll:1}}
+
+  MyCollection.find(ItemObj, ItemProjection).toArray((error,result)=>{
+   console.log(result)
+})
+}
+// Find by query
+function FindAllByQuery(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  var MyCollection = MyDB.collection('students')
+
+  const Query ={Roll:"01", Class:"Four"}
+
+  MyCollection.find(Query).toArray((error,result)=>{
+  if(error){
+    console.log('Error')
+    }else{
+    console.log(result);
+    }
+  })
+  }
+
+  // find by Limit
+  function FindDataByLimit(MyMongoClient){
+    var MyDB = MyMongoClient.db('school')
+    var MyCollection = MyDB.collection('students')
+      MyCollection.find().limit(30).toArray((error,result)=>{
+     if(error){
+      console.log('Error')
+      }else{
+      console.log(result);
+      }
+    })
+    }
+
+    // find All Data by Sort
+  function FindDataBySort(MyMongoClient){
+    var MyDB = MyMongoClient.db('school')
+    var MyCollection = MyDB.collection('students')
+      MyCollection.find().sort({Roll:-1}).toArray((error,result)=>{
+     if(error){
+      console.log('Error')
+      }else{
+      console.log(result);
+      }
+    })
+    }
+
+  // Update one Data 
+  function UpdateOneData(MyMongoClient){
+    var MyDB = MyMongoClient.db('school')
+    var MyCollection = MyDB.collection('students')
+      MyCollection.updateOne({Roll:"04"} , {$set:{name:"Rok",Class:"N"}},(error,result)=>{
+     if(error){
+      console.log('Error')
+      }else{
+      console.log("Update success" + result);
+      }
+    })
+    }
+
+// create new collection
+
+function CreateCollection(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  MyDB.createCollection("students2",(error,result)=>{
+     console.log(result) })
+
+  }
+
+// Drop collection
+
+function DropCollection(MyMongoClient){
+  var MyDB = MyMongoClient.db('school')
+  MyDB.dropCollection("students2",(error,result)=>{
+     console.log(result) })
+
+  }
