4e55e81a65ac22062f9372ba0d093a58306aa783
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2020-11-14 12:45:24 +0600

    LightSwitch using useState()

--- React JS Project/Mini Project/src/App.js
+++ React JS Project/Mini Project/src/App.js
@@ -1,80 +1,30 @@
-import React from 'react'
-import HackerNews from './Components/HackerNews'
-
+import React,{useState} from 'react'
+import './App.css'
 export default function App() {
+const [count, setCount] = useState({volume: 20,
+                                    bass: 20,
+                                    mid: 20,
+                                    treble: 20
+                                    })                       
     return (
         <>
-           <HackerNews info={realInfo}/> 
+            <Row count={count.volume} setCount={setCount} name="volume"/>
+            <Row count={count.bass} setCount={setCount} name="bass"/>
+            <Row count={count.mid} setCount={setCount} name="mid"/>
+            <Row count={count.treble} setCount={setCount} name="treble"/>
         </>
     )
 }
+const Up = ({count,setCount}) => <button onClick={console.log(count)}>+</button>
+const Down = ({count,setCount}) => <button onClick={()=>setCount(count-1)}>-</button>
 
-const realInfo = [
-    {
-        heading : 'Postgres Observability',
-        person  : 'fforflo',
-        link    : 'pgstats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 3 ,
-            comment : 22,
-            point   : 11
-        }
-    },
-    {
-        heading : 'Apple\'s Shifting Differentiation',
-        person  : 'osdfrflo',
-        link    : 'tats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 66 ,
-            comment : 32,
-            point   : 31
-        }
-    },
-    {
-        heading : 'es servability',
-        person  : 'sorflo',
-        link    : 'aastats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 31 ,
-            comment : 2,
-            point   : 21
-        }
-    },
-    {
-        heading : 'Postgres Observability',
-        person  : 'fforlo',
-        link    : 'pgstats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 3 ,
-            comment : 22,
-            point   : 11
-        }
-    },
-    {
-        heading : 'Apple\'s Shifting Differentiation',
-        person  : 'osdrflo',
-        link    : 'tats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 66 ,
-            comment : 32,
-            point   : 31
-        }
-    },
-    {
-        heading : 'es servability',
-        person  : 'sorfo',
-        link    : 'aastats.dev',
-        time    : '2020-11-11T14:29:40+00:00',
-        status  : {
-            upvote  : 31 ,
-            comment : 2,
-            point   : 21
-        }
-    }
-]
-
+const Row = ({count,setCount,name}) => {
+   return <div className="main">
+        <Up setCount={setCount} count={count}/>
+        <div className="cont">
+            <div className="ele">{count}</div>
+            <div className="ele">{name}</div>
+        </div>
+        <Down setCount={setCount} count={count}/>
+    </div>
+}
---
+++ React JS Project/Mini Project/src/Components/Demo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Demo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
