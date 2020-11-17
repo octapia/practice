38b89e74c4ae1030d6402a8af6a3719a2b253a9e
iftakher-t <67021268+iftakher-t@users.noreply.github.com>
2020-11-17 09:02:28 +0600

    Update app.js

--- express-project/app.js
+++ express-project/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from'mongoose'
 import morgan from 'morgan'
+// import User from './models'
 const app = express()
 
 
@@ -9,17 +10,23 @@ app.use(express.urlencoded({extended: true }))
 app.use(express.json())
 
 // schema part
+let Schema = mongoose.Schema
+let testSchema = new Schema({
+    name : String
+})
+
+let Test = mongoose.model('Test' , testSchema)
+
 app.get('/', (req,res)=>{
-       let users = new User({
+       let test = new Test({
         name : 'Iftakher'
     })
-    users.save()
-    .then(()=>{
-        app.listen(PORT,()=>{
-        console.log(`Server is running on ${PORT}`)
-    })
-    })
-    .catch(e =>{
+    test.save()
+
+        .then( t=>{
+        res.json(t)
+        })
+        .catch(e =>{
         console.log(e)
         res.status(500).json({
             error : 'Error occure'
@@ -28,12 +35,20 @@ app.get('/', (req,res)=>{
 })
 // connection 
 const PORT = process.env.PORT||3000
-mongoose.connect(`mongodb://127.0.0.1:27017/`, {
+mongoose.connect(`mongodb://127.0.0.1:27017`, {
     useNewUrlParser:true, useUnifiedTopology:true
     })
 .then(()=>{
     app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
+})
+})
+.catch(e =>{
+    console.log(e)
+})
+
+
+
 })
 })
 .catch(e =>{
