5d4ba891bc0a45e159e14e7081bdc3ea2e3e924d
iftakher-t <67021268+iftakher-t@users.noreply.github.com>
2020-11-19 09:08:09 +0600

    Update contactController.js

--- projectsl7/contactController.js
+++ projectsl7/contactController.js
@@ -1,11 +1,17 @@
+  
 const contacts =require('./Contacts')
 
 exports.getAllContacts = (req,res)=>{
     res.json(contacts.getAllContacts())
 }
 
-// exports.getContactsById = (req,res)=>{
-// }
+exports.getContactsById = (req,res)=>{
+    
+    let { id } = req.params
+    id = parseInt(id)
+    let contact = contacts.getContactsById(id)
+    res.json(contact)
+}
 
 exports.createContact = (req,res)=>{
     let { name ,email, phone} = req.body
@@ -15,9 +21,24 @@ exports.createContact = (req,res)=>{
         res.json(contact)
 }
 
-// exports.updateContactById= (req,res)=>{
-
-// }
-// exports.deleteContactById = (req,res)=>{
+exports.updateContactById = (req,res)=>{
+    let { id } = req.params
+    id = parseInt(id)
     
-// }
+    let { name ,email, phone} = req.body
+    let contact = contacts.updateContactById({
+          id,
+          name,
+          email,
+          phone
+        })
+        res.json(contact)
+}
+
+exports.deleteContactById = (req,res)=>{
+    let { id } = req.params
+    id = parseInt(id)
+
+    let contact = contacts.deleteContactById(id)
+    res.json(contact)
+}
