f5848ae5df54a706f8faea48d225781559f954f7
iftakher-t <67021268+iftakher-t@users.noreply.github.com>
2020-11-19 09:07:22 +0600

    Update contactRoutes.js

--- projectsl7/contactRoutes.js
+++ projectsl7/contactRoutes.js
@@ -1,17 +1,18 @@
+  
 const router = require('express').Router()
 
 const{
     getAllContacts,
-    // getContactsById,
-    createContact
-    // updateContactById,
-    // deleteContactById
+    getContactsById,
+    createContact,
+    updateContactById,
+    deleteContactById
     } = require('./contactController')
 
 router.get('/', getAllContacts)
-// router.get('/:id',getContactsById)
+router.get('/:id',getContactsById)
 router.post('/', createContact)
-// router.put('/:id',updateContactById)
-// router.delete('/:id',deleteContactById)
+router.put('/:id',updateContactById)
+router.delete('/:id',deleteContactById)
 
 module.exports = router
