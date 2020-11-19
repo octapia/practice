7838864007075676cdab4c3b7d77c923df61a7f4
iftakher-t <67021268+iftakher-t@users.noreply.github.com>
2020-11-19 09:05:59 +0600

    Update Contacts.js

--- projectsl7/Contacts.js
+++ projectsl7/Contacts.js
@@ -17,12 +17,12 @@ class Contacts {
         return contact
     }
 
-    updateContactById( id, updatedContact ){
-        let index = this.contacts.fiendIndex(contact => contact.id ===id)
+    updateContactById( {id, name,email,phone} ){
+        let index = this.contacts.findIndex(contact => contact.id == id)
        
-        this.contacts[index].name =updatedContact.name || this.contacts[index].name
-        this.contacts[index].email =updatedContact.email || this.contacts[index].email
-        this.contacts[index].phone =updatedContact.phone || this.contacts[index].phone
+        this.contacts[index].name = name || this.contacts[index].name
+        this.contacts[index].email = email || this.contacts[index].email
+        this.contacts[index].phone = phone || this.contacts[index].phone
        
         return this.contacts[index]
     }
